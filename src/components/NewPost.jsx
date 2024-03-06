6d69f329b834f436b2e5092bad9d648f0ba34656
yunbo-z <zyunbo0330@gmail.com>
2024-03-06 18:16:29 +0100

    update

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,18 +1,41 @@
+import { useState } from "react"
 import styles from "./NewPost.module.css"
 
 const NewPost = (props) => {
+    const [endertedbody, setEnteredBody] = useState('')
+    const [enteredAuther, setEnteredAuther] = useState('')
+
+    const bodyChangeHandler = (event) => {
+        setEnteredBody(event.target.value)
+    }
+    const autherChangeHandler = (event) => {
+        setEnteredAuther(event.target.value)
+    }
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const postData = {
+            body: endertedbody,
+            auther: enteredAuther
+        };
+        console.log(postData);
+        props.onAdd(postData)
+        props.onCancel()
+    }
 
     return(
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={submitHandler}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} onChange={props.onBodyChange}></textarea>
+                <textarea id="body" required rows={3} onChange={bodyChangeHandler}></textarea>
             </p>
-            
             <p>
                 <label htmlFor="name">Your name</label>
-                <input type="text" id="name" required onChange={props.onAutherChange}></input>
+                <input type="text" id="name" required onChange={autherChangeHandler}></input>
             </p>
+            <div className={styles.buttons}>
+                <button type="button" onClick={props.onCancel}>Cancel</button>
+                <button type="submit">Submit</button>
+            </div>
         </form>
     )
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,31 +3,63 @@ import NewPost from "./NewPost";
 import Modal from "./Modal";
 
 import styles from "./PostList.module.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const PostList = (props) => {
-    const [endertedbody, setEnteredBody] = useState('')
-    const [enteredAuther, setEnteredAuther] = useState('')
-    
-    const bodyChangeHandler = (event) => {
-        setEnteredBody(event.target.value)
-    }
-    const autherChangeHandler = (event) => {
-        setEnteredAuther(event.target.value)
+    const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false)
+
+    useEffect(() => {
+        async function fetchPosts() {
+            setIsFetching(true)
+            const response = await fetch('http://localhost:8080/posts')
+            //json method taking JSON as input,but the result is a js object
+            const resData = await response.json()
+            setPosts(resData.posts)
+            setIsFetching(false)
+        }
+        fetchPosts()
+    }, []);
+
+    const addPostHandler = (postData) => {
+        fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        //updating state based on the previous state.
+        setPosts((existingPosts) => [postData, ...existingPosts]);
     }
 
     return (
         <>
             {props.isPosting && (
                 <Modal onClose={props.onStopPosting}>
-                    <NewPost onBodyChange={bodyChangeHandler} onAutherChange={autherChangeHandler} />
+                    <NewPost
+                        onCancel={props.onStopPosting}
+                        onAdd={addPostHandler} />
                 </Modal>
             )}
-
-            <ul className={styles.posts}>
-                <Post auther={enteredAuther} body={endertedbody} />
-                <Post auther="Thomas" body="I like McDonald!" />
-            </ul>
+            {!isFetching && posts.length > 0 && (
+                <ul className={styles.posts}>
+                    {posts.map(
+                        (post) => <Post key={post.body} auther={post.auther} body={post.body} />
+                    )}
+                </ul>
+            )}
+            {!isFetching && posts.length == 0 && (
+                <div className={styles.noPost}>
+                    <h2>There are no posts yet.</h2>
+                    <p>Start adding some!</p>
+                </div>
+            )}
+            {isFetching && (
+                <div style={{ textAlign: 'center' }}>
+                    <p>Loading Post...</p>
+                </div>
+            )}
         </>
     )
 }
